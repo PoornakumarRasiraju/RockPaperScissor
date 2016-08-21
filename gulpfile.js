622f4130db372ba95030a79c2d6264bd62b0bef0
Poornakumar Rasiraju <tekzenit-prasiraju@ooyala.com>
2016-08-21 00:15:30 -0500

    Initial Commit with basic html and css view

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Plugins
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+// Compile Sass
+gulp.task('sass', function() {
+    return gulp.src('static/stylesheets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('static/stylesheets'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('static/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('static/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('static/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('static/js/*.js', ['lint', 'scripts']);
+    gulp.watch('static/stylesheets/scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['sass', 'scripts', 'watch']);
