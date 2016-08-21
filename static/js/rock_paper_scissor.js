03e3beee22c4018eb48116c28cc5d5a52e6415c5
Poornakumar Rasiraju <tekzenit-prasiraju@ooyala.com>
2016-08-21 11:47:51 -0500

    Added Scoreboard Values

--- static/js/rock_paper_scissor.js
+++ static/js/rock_paper_scissor.js
@@ -11,6 +11,12 @@
             $inputSeconds = $('.game_rps-sec-input', $game),
             $gameStart = $('.game_rps-start', $game),
             $gameRestart = $('.game_rps-restart', $game),
+            $hwin = $('.game_rps-h-sb-win', $game),
+            $hlose = $('.game_rps-h-sb-lose', $game),
+            $hdraw = $('.game_rps-h-sb-draw', $game),
+            $bwin = $('.game_rps-b-sb-win', $game),
+            $blose = $('.game_rps-b-sb-lose', $game),
+            $bdraw = $('.game_rps-b-sb-draw', $game),
             $humanRock = $('.game_rps-h-rock', $game),
             $humanPaper = $('.game_rps-h-paper', $game),
             $humanScissors = $('.game_rps-h-scissor', $game);
@@ -20,8 +26,14 @@
         });
 
         $gameRestart.on('click', function(e) {
-            var minutesVal = $inputMinutes.val(),
-                secondsVal = $inputSeconds.val();
+            /*var minutesVal = $inputMinutes.val(),
+                secondsVal = $inputSeconds.val();*/
+            $hwin.val('0');
+            $hlose.val('0');
+            $hdraw.val('0');
+            $bwin.val('0');
+            $blose.val('0');
+            $bdraw.val('0');
         });
 
         $humanRock.on('click', function(e) {
