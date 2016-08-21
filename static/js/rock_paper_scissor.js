32b8f3a207995f14dbf73edb34281566d8ec7dab
Poornakumar Rasiraju <tekzenit-prasiraju@ooyala.com>
2016-08-21 09:01:02 -0500

    Added Scoreboard html

--- static/js/rock_paper_scissor.js
+++ static/js/rock_paper_scissor.js
@@ -7,9 +7,22 @@
 (function($) {
     $(function() {
         var $game = $('.game'),
-            $humanRock = $('.game_rps-rock', $game),
-            $humanPaper = $('.game_rps-paper', $game),
-            $humanScissors = $('.game_rps-scissor', $game);
+            $inputMinutes = $('.game_rps-min-input', $game),
+            $inputSeconds = $('.game_rps-sec-input', $game),
+            $gameStart = $('.game_rps-start', $game),
+            $gameRestart = $('.game_rps-restart', $game),
+            $humanRock = $('.game_rps-h-rock', $game),
+            $humanPaper = $('.game_rps-h-paper', $game),
+            $humanScissors = $('.game_rps-h-scissor', $game);
+
+        $gameStart.on('click', function(e) {
+            console.log("Clicked Rock");
+        });
+
+        $gameRestart.on('click', function(e) {
+            var minutesVal = $inputMinutes.val(),
+                secondsVal = $inputSeconds.val();
+        });
 
         $humanRock.on('click', function(e) {
             console.log("Clicked Rock");
