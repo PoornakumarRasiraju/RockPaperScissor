709ad7d32ca5716cdc6227967f1c2f882d5310e5
Poornakumar Rasiraju <tekzenit-prasiraju@ooyala.com>
2016-08-21 19:03:10 -0500

    Added JS Files

---
+++ static/js/load_module.js
@@ -0,0 +1,95 @@
+
+var loadModule = (function (module) {
+    var defaults = {
+        buttonRestart: 'game_rps-restart',
+        buttonStart: 'game_rps-start',
+        result: 'game-result',
+        buttonSelect: 'btn-select',
+        botSelected: 'bot-select',
+        winCount: 'sb-win',
+        looseCount: 'sb-loose',
+        drawCount: 'sb-draw',
+        warningMessage: 'Just warning',
+        winMessage: '<strong>Congtrats you won</strong>',
+        looseMessage: '<strong>Oops</strong> you losed try again!',
+        drawMessage: '<strong>Match is drawn</strong>'
+    }
+
+    $('#' + module.defaults.inputMinuteId).on('keypress', module.numericKeyPressEvent);
+
+    $('#' + module.defaults.inputSecondId).on('keypress', module.numericKeyPressEvent);
+
+    $('.' + defaults.buttonStart).on('click', function(){
+        module.start(timerChange);
+    });
+
+    $('.' + defaults.buttonRestart).on('click', function () {
+        module.resetInterval();
+        $('.' + defaults.winCount).text(0);
+        $('.' + defaults.looseCount).text(0);
+        $('.' + defaults.drawCount).text(0);
+        module.start(timerChange);
+    });
+
+    $('.' + defaults.buttonSelect).on('click', function () {
+        var userSelected = $(this).text().toLowerCase();
+        var botSelected = module.botPickchoice();
+        $('.' + defaults.botSelected).text(botSelected);
+        var result = module.compare(userSelected, botSelected);
+
+        if (result === "tie") {
+            var drawEle = $('.' + defaults.drawCount);
+            var d = parseInt(drawEle.text());
+            if (isNaN(d)) {
+                d = 0;
+            }
+            d++;
+            drawEle.text(d);
+        }
+        else if (userSelected === result) {
+            var winEle = $('.' + defaults.winCount);
+            var w = parseInt(winEle.text());
+            if (isNaN(w)) {
+                w = 0;
+            }
+            w++;
+            winEle.text(w);
+        }
+        else {
+            var looseEle = $('.' + defaults.looseCount);
+            var l = parseInt(looseEle.text());
+            if (isNaN(l)) {
+                l = 0;
+            }
+            l++;
+            looseEle.text(l);
+        }
+
+    })
+
+    var showWin = function () {
+        var winCount = parseInt($('.' + defaults.winCount).text());
+        var looseCount = parseInt($('.' + defaults.looseCount).text());
+        var drawCount = parseInt($('.' + defaults.drawCount).text());
+
+        if (winCount > looseCount) {
+            $('.' + defaults.result).html(defaults.winMessage).removeClass('hide');
+        }
+        else if (looseCount > winCount) {
+            $('.' + defaults.result).html(defaults.looseMessage).removeClass('hide');
+        }
+        else if (winCount == looseCount && drawCount > 0) {
+            $('.' + defaults.result).html(defaults.drawMessage).removeClass('hide');
+        }
+    }
+
+    function timerChange(timer, minutes, seconds) {
+        console.log("Mins: " + minutes + " Sec: " + seconds);
+        if(timer === 10){
+            alert(defaults.warningMessage);
+        }
+        if(timer < 0){
+            showWin();
+        }
+    }
+})(rockModule);
---
+++ static/js/rock_module.js
@@ -0,0 +1,116 @@
+var rockModule = (function () {
+
+    var timeInterval;
+    var choices = ["rock", "paper", "scissors"];
+    var map = {};
+    var defaults = {
+        displayMinute: '.game_rps-timer-min',
+        displaySecond: '.game_rps-timer-sec',
+        inputMinuteClass: 'game_rps-min-input',
+        inputSecondClass: 'game_rps-sec-input'
+    };
+    choices.forEach(function (choice, i) {
+        map[choice] = {};
+        for (var j = 0, half = (choices.length - 1) / 2; j < choices.length; j++) {
+            var opposition = (i + j) % choices.length;
+            if (!j)
+                map[choice][choice] = "tie";
+            else if (j <= half)
+                map[choice][choices[opposition]] = choices[opposition];
+            else
+                map[choice][choices[opposition]] = choice;
+        }
+    });
+
+    var compare = function (choice1, choice2) {
+        return (map[choice1] || {})[choice2] || "Invalid choice";
+    };
+
+    var botPickchoice = function () {
+        return choices[Math.floor(Math.random() * choices.length)];
+    };
+
+    var numericKeyPressEvent = function (event) {
+        // Backspace, tab, enter, end, home, left, right
+        var controlKeys = [8, 9, 13, 35, 36, 37, 39];
+        var isControlKey = controlKeys.join(",").match(new RegExp(event.which));
+        if (!event.which ||
+            (49 <= event.which && event.which <= 57) ||
+            (48 == event.which && $(this).val() > 0) ||
+            isControlKey) {
+            return;
+        } else {
+            event.preventDefault();
+        }
+    }
+
+    var startTimer = function (duration, displayMinute, displaySecond, timerChangeCallback) {
+        var timer = duration, displayMinute, displaySecond;
+        timeInterval = setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            displayMinute.textContent = minutes;
+            displaySecond.textContent = seconds;
+
+            timerChangeCallback && timerChangeCallback.call(null, timer, minutes, seconds);
+            if (--timer < 0) {
+                timerChangeCallback && timerChangeCallback.call(null, timer, minutes, seconds);
+                timer = 0;
+                if (timer == 0) {
+                    resetInterval();
+                }
+            }
+        }, 1000);
+    }
+
+    var resetInterval = function () {
+        if (timeInterval) {
+            clearInterval(timeInterval);
+        }
+    }
+
+    var start = function (timerChangeCallback) {
+        resetInterval();
+        var fragmentTime;
+
+        var minutes = $('.' + defaults.inputMinuteClass).val();
+        var seconds = $('.' + defaults.inputSecondClass).val();
+
+        minutes = parseInt(minutes);
+        seconds = parseInt(seconds);
+
+        if (isNaN(minutes)) {
+            minutes = 00;
+        }
+        if (isNaN(seconds)) {
+            seconds = 00;
+        }
+
+        if (minutes == 60) {
+            minutes = 59;
+        }
+        if (seconds == 60) {
+            seconds = 59;
+        }
+
+        fragmentTime = (60 * minutes) + (seconds);
+        displayMinute = document.querySelector(defaults.displayMinute);
+        displaySecond = document.querySelector(defaults.displaySecond);
+
+        startTimer(fragmentTime, displayMinute, displaySecond, timerChangeCallback);
+    }
+
+    return {
+        defaults: defaults,
+        start: start,
+        compare: compare,
+        botPickchoice: botPickchoice,
+        resetInterval: resetInterval,
+        numericKeyPressEvent: numericKeyPressEvent
+    };
+
+})();
